2480fb7365ffc3334d43d301d85e191cad606393
dbrudals <112334565+dbrudals922@users.noreply.github.com>
2023-12-21 20:54:35 +0900

    Update baseballGameJs.js

--- baseballGameJs.js
+++ baseballGameJs.js
@@ -55,7 +55,7 @@ function setNumber() {
         check = true;
     }
 
-    // console.log(answerNum);
+    console.log(answerNum);
     // document.getElementById("answer").innerText = getNum;
 }
 
@@ -218,7 +218,7 @@ function showResultBall(userNum) {
                     // document.getElementsByClassName('result')[0].innerText = null;
                 } else if (document.getElementsByClassName("result-item").length >= 9) {
                     clearInterval(Interval);
-                    alert("실패!");
+                    alert("실패!\n 정답은 "+ answerNum.join()+ "이었습니다ㅠㅠ");
                     history.go(0);
                     // document.getElementsByClassName('result')[0].innerText = null;
                 }
